846df72b574d303faea09763c355c1ecfa29e39c
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-02-12 02:05:39 -0800

    Actualizar Sectionex.lua

--- Modules/Sectionex.lua
+++ Modules/Sectionex.lua
@@ -4,6 +4,7 @@ local Tab = _G.SettingsTab
 local Section = Tab:Section({ 
     Title = "Section",
     Box = false,
+    FontWeight = "SemiBold",
     TextTransparency = 0.05,
     TextXAlignment = "Left",
     TextSize = 17, -- Default Size
