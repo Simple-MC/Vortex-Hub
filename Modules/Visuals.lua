4008e86366a3394be0e12e775e8f93f48cc495f5
SimpleStew007 <daniellopezgerardo02e@gmail.com>
2026-03-01 23:46:24 -0800

    Update Visuals.lua

--- Modules/Visuals.lua
+++ Modules/Visuals.lua
@@ -19,7 +19,10 @@ local function LimpiarTexto(texto)
 end
 
 local function buscar()
-    local m = workspace:FindFirstChild("GameObjects") and GameObjects:FindFirstChild("PlaceSpecific") and PlaceSpecific:FindFirstChild("root") and root:FindFirstChild("EventTimers")
+    local lo = workspace:FindFirstChild("GameObjects")
+    local la = lo:FindFirstChild("PlaceSpecific")
+    local le = la:FindFirstChild("root")
+    local m = le:FindFirstChild("EventTimers")
     if not m then return end
     
     for _, p in pairs(m:GetChildren()) do
